b824e159b97520a8121096fd3c0ad69d18f3db6d
1ethon0822 <1ethyjj9240@gmail.com>
2026-01-20 19:05:53 +0900

    26/01/20 오후

--- src/ch07/sec08/BullDog.java
+++ src/ch07/sec08/BullDog.java
@@ -3,6 +3,6 @@ package ch07.sec08;
 public class BullDog extends  Dog{
     @Override
     public void crying(){
-        System.out.println("불독이 월~월~ ");
+         System.out.println("불독이 월~월~ ");
     }
 }
